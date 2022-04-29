239e735b85d79bc9ddf7b3fd574f69bcd96d5d2e
Sabrina Godoy <sabrinapenteadogodoy2003@gmail.com>
2022-04-29 14:09:57 -0300

    blog pessoal

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -8,4 +8,5 @@ import com.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
 }
